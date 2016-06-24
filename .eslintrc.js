d3098cc49a8cc8ca810e7b6732f83164100765a7
Nazmuz Shakib Pranto <npranto@gmail.com>
2016-06-24 11:09:57 -0500

    test

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    // "env": {
+    //     "browser": true,
+    //     "commonjs": true,
+    //     "es6": true,
+    //     "node": true
+    // },
+    // parserOptions:{
+    //     ecmaVersion: 6
+    // },
+    // globals:["angular"],
+    // "extends": "eslint:recommended",
+    // "rules": {
+    //     "indent": [
+    //         "error",
+    //         "tab"
+    //     ],
+    //     "linebreak-style": [
+    //         "error",
+    //         "unix"
+    //     ],
+    //     "quotes": [
+    //         "error",
+    //         "double"
+    //     ],
+    //     "semi": [
+    //         "warn",
+    //         "always"
+    //     ],
+    //     "no-use-before-define": "warn",
+    // }
+    extends: "devmountain/es6-config"
+};
+
+
